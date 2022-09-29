97545a972c71664d33d39ae1a3552301124f3b99
bastian <bastianalzate1@gmail.com>
2022-09-29 09:38:56 -0500

    modify package.json and vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/images/logo.png',
-                'resources/images/dev.png',
             ],
             refresh: true,
         }),
